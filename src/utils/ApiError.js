10488d268fded73ba559ae24c7887be0be359a5c
Vasu <vasukumar962@gmail.com>
2024-05-15 16:55:18 +0530

    set multer

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,17 +3,17 @@ class ApiError extends Error {
     statusCode,
     message = "Something went wrong",
     errors = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     this.statusCode = statusCode;
     this.data = null;
     this.message = message;
     this.success = false;
-    this.errors = this.errors;
+    this.errors = errors;
 
-    if (statck) {
-      this.stack = statck;
+    if (stack) {
+      this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
